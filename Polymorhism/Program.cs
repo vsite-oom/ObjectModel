d54602d628a3cfc7805d64a6c5ec2920d7fdab8e
Julijan Sribar <julijan.sribar@gmail.com>
2023-03-15 20:08:43 +0100

    Implement text formatting

--- Polymorhism/Program.cs
+++ Polymorhism/Program.cs
@@ -6,9 +6,15 @@ namespace Vsite.Oom.ObjectModel
     {
         static void Main(string[] args)
         {
-            Console.OutputEncoding = Encoding.UTF8;
+            ITextFormatter formatter = new PlainTextFormatter();
 
-            StoryTeller st = new StoryTeller();
+            Console.WriteLine("Do you want in HTML? (y/n)");
+            if (Console.ReadLine() == "y")
+            {
+                formatter = new HtmlTextFormatter();
+            }
+
+            StoryTeller st = new StoryTeller(formatter);
             st.WriteHeading1("Snow White and the wolf");
             st.WriteHeading2("Leaving home");
             st.WriteParagraph("Once upon a time, in a land far, far away...");
