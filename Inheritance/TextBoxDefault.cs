481d3f8b689f1c849de5ea82ef535ace4b2d7c04
Nedim Omerović <neomerov@vsite.hr>
2023-03-15 19:33:06 +0100

    Create TextBox default Control inherited from textbox

---
+++ Inheritance/TextBoxDefault.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.ObjectModel
+{
+    internal class TextBoxDefault : TextBox
+    {
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            SetStyle(ControlStyles.UserPaint, TextLength == 0);
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            if (Text.Length == 0)
+            {
+                DrawDefaultText(e.Graphics);
+            }
+        }
+        protected override void OnTextChanged(EventArgs e)
+        {
+            base.OnTextChanged(e);
+            
+            SetStyle(ControlStyles.UserPaint, TextLength == 0);
+            Invalidate();
+
+        }
+        private void DrawDefaultText(Graphics graphics)
+        {
+            TextRenderer.DrawText(graphics, defaultText, Font, ClientRectangle, 
+                SystemColors.GrayText, TextFormatFlags.TextBoxControl);
+        }
+
+        private string defaultText = "Default value";
+    }
+}
