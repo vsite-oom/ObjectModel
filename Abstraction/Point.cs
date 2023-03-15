7af5284fc30f6ccd3904220e6d7b0eb7b8fff964
Šime Šprljan <sisprlja@vsite.hr>
2023-03-15 19:08:33 +0100

    Create Line and Point abstractions

---
+++ Abstraction/Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.ObjectModel
+{
+    internal class Point
+    {
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        public readonly double X;
+        public readonly double Y;
+    }
+}
