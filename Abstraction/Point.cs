20fbaa80abafa1c808efd47eef63b576aee5e672
Giancarlo <giancarlo.bedek@hotmail.com>
2023-03-15 19:08:15 +0100

    Create Line and Point abstractions

---
+++ Abstraction/Point.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.ObjectModel
+{
+    internal struct Point
+    {
+        public readonly double X;
+        public readonly double Y;
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"x: {X}\ny: {Y}";
+        }
+    }
+}
