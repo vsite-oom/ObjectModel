8e3f505f11cf0dd79d75b9e4973e77242db0b989
FranKalinger0998 <f.kalinger@gmail.com>
2023-03-15 20:08:45 +0100

    Implemented text formating

--- Abstraction/Program.cs
+++ Abstraction/Program.cs
@@ -17,29 +17,7 @@
             {
                 Console.WriteLine($"Intersection: ({res?.X}, {res?.Y})");
             }
-            //try
-
-            //{
-            //    double k1 = double.Parse(args[0]);
-            //    double l1 = double.Parse(args[1]);
-            //    double k2 = double.Parse(args[2]);
-            //    double l2 = double.Parse(args[3]);
-
-            //    if (k1 == k2)
-            //    {
-            //        Console.WriteLine("Lines are parallel");
-            //    }
-            //    else
-            //    { 
-            //        double x = (l2 - l1) / (k1 - k2);
-            //        double y = k1 * x + l1;
-            //        Console.WriteLine($"Intersection: ({x}, {y})");
-            //    }
-            //}
-            //catch (Exception)
-            //{
-            //    Console.WriteLine("Invalid entry");
-            //}
+           
         }
     }
 }
