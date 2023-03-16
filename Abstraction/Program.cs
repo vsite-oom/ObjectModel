71786c96e1b0bbcd1b61ab5efb2339c794279725
Julijan Sribar <julijan.sribar@gmail.com>
2023-03-16 18:49:02 +0100

    Create abstraction for point and line

--- Abstraction/Program.cs
+++ Abstraction/Program.cs
@@ -11,15 +11,17 @@
                 double k2 = double.Parse(args[2]);
                 double l2 = double.Parse(args[3]);
 
-                if (k1 == k2)
+                var line1 = new Line(k1, l1);
+                var line2 = new Line(k2, l2);
+
+                var result = line1.Intersection(line2);
+                if (result == null)
                 {
                     Console.WriteLine("Lines are parallel");
                 }
                 else
                 {
-                    double x = (l2 - l1) / (k1 - k2);
-                    double y = k1 * x + l1;
-                    Console.WriteLine($"Intersection: ({x}, {y})");
+                    Console.WriteLine($"Intersection: ({result?.X}, {result?.Y})");
                 }
             }
             catch (Exception)
