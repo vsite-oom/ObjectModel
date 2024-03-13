c573730705a88a8a57cbf8e349de82fd3240435f
Ivan Selendic <ivan.selendic@gmail.com>
2024-03-13 20:26:22 +0100

    Encapsulate line

---
+++ Abstraction/Line.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.ObjectModel
+{
+    internal class Line
+    {
+        public Line(double k, double l)
+        {
+            K = k;
+            L = l;
+
+        }
+        public readonly double K;
+        public readonly double L;
+
+        public Point? Intersection(Line other)
+        {
+            if (K == other.K)
+            {
+                return null;
+            }
+            double x = (other.L - L) / (K- other.K);
+            double y = K * x + L;
+            return new Point(x, y);
+        }
+    }
+}
