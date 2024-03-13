6718902c888d9ca1139891f79e578c9da34a3ef0
Kristian Mesić <kristian.mesic@king-ict.eu>
2024-03-13 20:54:45 +0100

    - check inheritance - add default value for texbox from our class that inherits textbox

---
+++ Inheritance/TextBoxDefault.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.ObjectModel
+{
+    internal class TextBoxDefault : TextBox
+    {
+        private readonly string defaultText = "Default value";
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            SetStyle(ControlStyles.UserPaint, TextLength == 0);
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            if (Text.Length == 0)
+            {
+                DrawDefaultText(e);
+            }
+        }
+
+        protected override void OnTextChanged(EventArgs e)
+        {
+            base.OnTextChanged(e);
+            SetStyle(ControlStyles.UserPaint, TextLength == 0);
+            if(Text.Length == 0)
+            {
+                Invalidate();
+            }
+        }
+
+        private void DrawDefaultText(PaintEventArgs e)
+        {
+            TextRenderer.DrawText(e.Graphics, defaultText, Font, ClientRectangle, SystemColors.GrayText, TextFormatFlags.TextBoxControl);
+        }
+    }
+}
