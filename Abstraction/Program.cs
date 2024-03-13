ba23601772002916320d7458ba7541f40eaef7c3
DrHrast <petar.huljek@gmail.com>
2024-03-13 20:26:10 +0100

    Encapsulate line.

--- Abstraction/Program.cs
+++ Abstraction/Program.cs
@@ -11,16 +11,20 @@
                 double k2 = double.Parse(args[2]);
                 double l2 = double.Parse(args[3]);
 
-                if (k1 == k2)
+                Line line1 = new Line(k1, l1);
+                Line line2 = new Line(k2, l2);
+
+                var intersection = line1.Intersection(line2);
+                if (intersection == null)
                 {
                     Console.WriteLine("Lines are parallel");
                 }
                 else
                 {
-                    double x = (l2 - l1) / (k1 - k2);
-                    double y = k1 * x + l1;
-                    Console.WriteLine($"Intersection: ({x}, {y})");
+                    Console.WriteLine($"Intersection: ({intersection?.X}, {intersection?.Y})");
                 }
+
+                
             }
             catch (Exception)
             {
