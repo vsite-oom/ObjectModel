027bb6efd5fa9765501fe753996ad695685d2228
Marko Pavić <mapavic@vsite.hr>
2024-03-13 20:55:22 +0100

    Implement derived texbox class

--- Inheritance/FormMain.cs
+++ Inheritance/FormMain.cs
@@ -1,18 +1,23 @@
 ﻿namespace Vsite.Oom.ObjectModel
 {
-    public partial class FormMain : Form
-    {
-        public FormMain()
-        {
-            InitializeComponent();
-        }
+	public partial class FormMain : Form
+	{
+		public FormMain()
+		{
+			InitializeComponent();
+		}
 
-        private void buttonOk_Click(object sender, EventArgs e)
-        {
-            if (MessageBox.Show("Are you sure to exit?", "Customer Information", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
-            {
-                Close();
-            }
-        }
-    }
+		private void buttonOk_Click(object sender, EventArgs e)
+		{
+			if (MessageBox.Show("Are you sure to exit?", "Customer Information", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+			{
+				Close();
+			}
+		}
+
+		private void FormMain_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
 }
