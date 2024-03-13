cbc557ec42a5436a163f5c52af15cd4763e55944
Mihael Krpes <mikrpes@vsite.hr>
2024-03-13 20:54:49 +0100

    Inheritance

---
+++ Inheritance/TextBoxDefault.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.ObjectModel
+{
+    internal class TextBoxDefault: TextBox
+    {
+        private string defaultText = "Def value";
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            SetStyle(ControlStyles.UserPaint, TextLength == 0);
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            if(TextLength==0 )
+            {
+                DrawDefaultText(e);
+            }
+        }
+        
+
+        private void DrawDefaultText(PaintEventArgs e)
+        {
+            TextRenderer.DrawText(e.Graphics,defaultText,Font,ClientRectangle,SystemColors.GrayText,TextFormatFlags.TextBoxControl);
+        }
+    }
+}
