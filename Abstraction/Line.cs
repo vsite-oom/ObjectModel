235515e63e4d943eeaec077fc4599f971f322941
Mihael Krpes <mikrpes@vsite.hr>
2024-03-13 20:25:50 +0100

    abstraction

---
+++ Abstraction/Line.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.ObjectModel
+{
+    internal class Line
+    {
+        public readonly double K;
+        public readonly double L;
+        public Line(double k, double l)
+        {
+            this.K = k;
+            this.L = l;
+        }
+        public Point? Intersection(Line other)
+        {
+            double x = (other.L - L) / (K - other.K);
+            double y = K * x + L;
+            return new Point(x, y);
+        }
+    }
+}
---
+++ Abstraction/Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.ObjectModel
+{
+    internal struct Point
+    {
+        public double X;
+        public double Y;
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
