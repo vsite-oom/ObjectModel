6b7115d4386ce5a638c37320eb8a35eb823c3c09
benjamin.gajski@gmail.com <Benjamin.Gajski@gmail.com>
2024-03-13 20:27:03 +0100

    1 - abstrakcija

--- Abstraction/Program.cs
+++ Abstraction/Program.cs
@@ -11,15 +11,18 @@
                 double k2 = double.Parse(args[2]);
                 double l2 = double.Parse(args[3]);
 
-                if (k1 == k2)
+                Line Line1 = new Line(k1, l1);
+                Line Line2 = new Line(k2, l2);
+
+                var intersection = Line1.Intersetcion(Line2);
+
+                if (intersection == null)
                 {
                     Console.WriteLine("Lines are parallel");
                 }
                 else
                 {
-                    double x = (l2 - l1) / (k1 - k2);
-                    double y = k1 * x + l1;
-                    Console.WriteLine($"Intersection: ({x}, {y})");
+                    Console.WriteLine($"Intersection: ({intersection?.X}, {intersection?.Y})");
                 }
             }
             catch (Exception)
