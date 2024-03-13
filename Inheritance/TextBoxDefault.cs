1fb7238d16cff8e89be32b00dfa9832f733d8392
Đuro Rukavina <djrukavina@vsite.hr>
2024-03-13 20:55:01 +0100

    Implement derived TextBoxDefault class

---
+++ Inheritance/TextBoxDefault.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.ObjectModel
+{
+    internal class TextBoxDefault : TextBox
+    {
+        private string defaultText = "Default value";
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            SetStyle(ControlStyles.UserPaint, TextLength == 0);
+        }
+
+        protected override void OnTextChanged(EventArgs e)
+        {
+            base.OnTextChanged(e);
+            SetStyle(ControlStyles.UserPaint, TextLength == 0);
+            if (TextLength == 0)
+            {
+                Invalidate();
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            if(TextLength == 0)
+            {
+                DrawDefaultText(e);
+            }
+        }
+
+        private void DrawDefaultText(PaintEventArgs e)
+        {
+            TextRenderer.DrawText(e.Graphics, defaultText, base.Font, ClientRectangle, SystemColors.GrayText, TextFormatFlags.TextBoxControl);
+        }
+    }
+}
