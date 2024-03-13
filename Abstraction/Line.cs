481beab81a8fe70120eab7d03836efdbb1791ee7
Đuro Rukavina <djrukavina@vsite.hr>
2024-03-13 20:24:47 +0100

    Encapsulate Line

---
+++ Abstraction/Line.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.ObjectModel
+{
+    internal class Line
+    {
+        public readonly double K;
+        public readonly double L;
+
+        public Line(double k, double l)
+        {
+            this.K = k;
+            this.L = l;
+        }
+
+        public Point? Intersection(Line other)
+        {
+            if (this.K == other.K) return null;
+            double x = (other.L - this.L) / (this.K - other.K);
+            double y = this.K * x + this.L;
+            return new Point(x, y);
+        }
+    }
+}
---
+++ Abstraction/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.ObjectModel
+{
+    internal struct Point
+    {
+        public readonly double X;
+        public readonly double Y;
+
+        public Point(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+}
