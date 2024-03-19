ebd4ad704cfbd5e7aa14e1026c407d0c1cb90596
Julijan Šribar <julijan.sribar@gmail.com>
2024-03-19 18:48:40 +0100

    Extend example for polymorphism

--- Polymorhism/Program.cs
+++ Polymorhism/Program.cs
@@ -9,9 +9,15 @@ namespace Vsite.Oom.ObjectModel
             Console.OutputEncoding = Encoding.UTF8;
 
             StoryTeller st = new StoryTeller();
-            st.WriteHeading1("Snow White and the wolf");
+            st.WriteHeading1("Snow White and the Wolf");
             st.WriteHeading2("She's leaving home");
             st.WriteParagraph("Once upon a time, in a land far, far away...");
+
+            st.WriteHeading2("A Chance Encounter with a Prince on White Horse");
+            st.WriteParagraph("She was dancing alone on the dancing floor...");
+
+            st.WriteHeading1("The End");
+            st.WriteParagraph("And they lived happily ever after...");
 
             Console.ReadKey(false);
         }
