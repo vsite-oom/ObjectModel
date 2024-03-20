17dfd00e30d986257af92795a62e2201883f9b00
Josip Grabušić <jograbus@vsite.hr>
2024-03-20 20:33:03 +0100

    implement formatters

---
+++ Polymorhism/HtmlTextFormatter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.ObjectModel
+{
+    internal class HtmlTextFormatter: ITextFormatter
+    {
+        public void WriteParagraph(string text)
+        {
+         
+                Console.WriteLine($"<p>{text}</p>");
+        }
+
+        public void WriteHeading1(string caption)
+        {
+           
+                Console.WriteLine($"<h1>{caption}</h1>");
+
+        }
+
+        public void WriteHeading2(string caption)
+        {
+         
+                Console.WriteLine($"<h2>{caption}</h2>");
+            
+        }
+
+    }
+}
