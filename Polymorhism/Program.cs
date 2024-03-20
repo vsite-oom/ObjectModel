a95f6bf8404cd12be75256eaeadd52d79b749a94
AnMal <junkie_@msn.com>
2024-03-20 20:04:28 +0100

    Implement HTML formatting

--- Polymorhism/Program.cs
+++ Polymorhism/Program.cs
@@ -8,7 +8,7 @@ namespace Vsite.Oom.ObjectModel
         {
             Console.OutputEncoding = Encoding.UTF8;
 
-            StoryTeller st = new StoryTeller();
+            StoryTeller st = new StoryTeller(true);
             st.WriteHeading1("Snow White and the Wolf");
             st.WriteHeading2("She's leaving home");
             st.WriteParagraph("Once upon a time, in a land far, far away...");
