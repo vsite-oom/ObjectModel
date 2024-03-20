0f3738f7e26b58e6b732802207f0603e80d70a35
benjamin.gajski@gmail.com <Benjamin.Gajski@gmail.com>
2024-03-20 20:33:08 +0100

    commit

---
+++ Polymorhism/HtmlTextFormater.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.ObjectModel
+{
+    internal class HtmlTextFormater : ITextFormatter
+    {
+        public void WriteParagraph(string text)
+        {
+
+                Console.WriteLine($"<p>(text)</p>");
+        }
+
+        public void WriteHeading1(string caption)
+        {
+            Console.WriteLine($"<h1>(caption)</h2>");
+        }
+        public void WriteHeading2(string caption)
+        {
+            Console.WriteLine($"<h2>(caption)</h2>");
+        }
+    }
+}
---
+++ Polymorhism/PlainTextFormater.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.ObjectModel
+{
+    internal class PlainTextFormater : ITextFormatter
+    {
+        public void WriteParagraph(string text)
+        {
+                Console.WriteLine(text);
+        }
+
+        public void WriteHeading1(string caption)
+        {
+
+                Console.WriteLine();
+
+                Console.WriteLine(caption);
+                Console.WriteLine(new string('=', caption.Length));
+        }
+
+        public void WriteHeading2(string caption)
+        {
+
+            Console.WriteLine();
+
+            Console.WriteLine(caption);
+            Console.WriteLine(new string('=', caption.Length));
+        }
+    }
+}
