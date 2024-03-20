1155cac5cabe867b6a254743f799faee2afe2f6b
benjamin.gajski@gmail.com <Benjamin.Gajski@gmail.com>
2024-03-20 20:47:03 +0100

    commit

--- Polymorhism/StoryTeller.cs
+++ Polymorhism/StoryTeller.cs
@@ -2,21 +2,33 @@
 {
     internal class StoryTeller
     {
+        public StoryTeller(ITextFormatter textFormatter)
+        {
+            this.textFormatter = textFormatter;
+        }
+
+        private readonly ITextFormatter textFormatter;
+
         public void WriteParagraph(string text)
         {
-            Console.WriteLine(text);
+            textFormatter.WriteParagraph(text);
         }
 
         public void WriteHeading1(string caption)
         {
-            Console.WriteLine(caption);
-            Console.WriteLine(new string('=', caption.Length));
+            ++heading1Number;
+            caption = $"{heading1Number}. {caption}";
+            textFormatter.WriteHeading1($"{caption}");
         }
 
         public void WriteHeading2(string caption)
         {
-            Console.WriteLine(caption);
-            Console.WriteLine(new string('-', caption.Length));
+            ++heading2Number;
+            caption = $"{heading1Number}.{heading2Number}. {caption}";
+            textFormatter.WriteHeading2($"{caption}");
         }
+
+        private int heading1Number = 0;
+        private int heading2Number = 0;
     }
 }
