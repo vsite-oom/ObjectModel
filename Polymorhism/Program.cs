09decccfbd6172563fe2427eaaf3169c89734a73
Mihael Krpes <mikrpes@vsite.hr>
2024-03-20 20:06:06 +0100

    Storyteller

--- Polymorhism/Program.cs
+++ Polymorhism/Program.cs
@@ -8,12 +8,12 @@ namespace Vsite.Oom.ObjectModel
         {
             Console.OutputEncoding = Encoding.UTF8;
 
-            StoryTeller st = new StoryTeller();
+            StoryTeller st = new StoryTeller(true);
             st.WriteHeading1("Snow White and the Wolf");
             st.WriteHeading2("She's leaving home");
             st.WriteParagraph("Once upon a time, in a land far, far away...");
 
-            st.WriteHeading2("A Chance Encounter with a Prince on White Horse");
+            st.WriteHeading2("A Chance Encounter with a Prince on a White Horse");
             st.WriteParagraph("She was dancing alone on the dancing floor...");
 
             st.WriteHeading1("The End");
