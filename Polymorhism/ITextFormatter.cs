3c001ebdcb75ee03ad42b3898023627cc65a29b1
Kristian Bolarić <krbolaric@vsite.hr>
2024-03-20 20:31:22 +0100

    Implement polymorphism

---
+++ Polymorhism/ITextFormatter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Oom.ObjectModel
+{
+    internal interface ITextFormatter
+    {
+        void WriteParagraph(string text);
+        void WriteHeading1(string caption);
+        void WriteHeading2(string caption);
+
+    }
+}
