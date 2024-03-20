70ab95fff44a9ba4e2f7463aed2d93dcd4e8e8e9
Đuro Rukavina <djrukavina@vsite.hr>
2024-03-20 20:04:30 +0100

    implement html formatting

--- Polymorhism/StoryTeller.cs
+++ Polymorhism/StoryTeller.cs
@@ -2,30 +2,55 @@
 {
     internal class StoryTeller
     {
-        public void WriteParagraph(string text)
+        private readonly bool html;
+        public StoryTeller(bool html = false)
         {
-            Console.WriteLine(text);
+            this.html = html;
+        }
+        public void WriteParagraph(string text, bool html)
+        {
+            if (html)
+            {
+                Console.WriteLine($"<p>{text}</p>");
+            }else
+            {
+                Console.WriteLine(text);
+            }
         }
 
-        public void WriteHeading1(string caption)
+        public void WriteHeading1(string caption, bool html)
         {
-            Console.WriteLine();
+            if(html)
+            {
+                Console.WriteLine($"<h1>{caption}</h1>");
+            }
+            else
+            {
+                Console.WriteLine();
 
-            ++heading1Number;
+                ++heading1Number;
 
-            caption = $"{heading1Number}. {caption}";
-            Console.WriteLine(caption);
-            Console.WriteLine(new string('=', caption.Length));
+                caption = $"{heading1Number}. {caption}";
+                Console.WriteLine(caption);
+                Console.WriteLine(new string('=', caption.Length));
+            }
         }
 
-        public void WriteHeading2(string caption)
+        public void WriteHeading2(string caption, bool html)
         {
-            Console.WriteLine();
+            if(!html)
+            {
+                Console.WriteLine($"<h2>{caption}</h2>");
+            }
+            else
+            {
+                Console.WriteLine();
 
-            ++heading2Number;
-            caption = $"{heading1Number}.{heading2Number}. {caption}";
-            Console.WriteLine(caption);
-            Console.WriteLine(new string('-', caption.Length));
+                ++heading2Number;
+                caption = $"{heading1Number}.{heading2Number}. {caption}";
+                Console.WriteLine(caption);
+                Console.WriteLine(new string('-', caption.Length));
+            }
         }
 
         private int heading1Number = 0;
