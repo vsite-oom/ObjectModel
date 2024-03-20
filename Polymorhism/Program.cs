83a383265a2cccaadcfad0c7dcf1adeb2f498ee3
Sonja Perković <soperkov@vsite.hr>
2024-03-20 20:31:47 +0100

    implemented plain and html text formatting interfaces

--- Polymorhism/Program.cs
+++ Polymorhism/Program.cs
@@ -8,7 +8,22 @@ namespace Vsite.Oom.ObjectModel
         {
             Console.OutputEncoding = Encoding.UTF8;
 
-            StoryTeller st = new StoryTeller(true);
+            ITextFormatter tf = null;
+
+            Console.WriteLine("Do you want in HTML format?");
+
+            string? answer = Console.ReadLine();
+
+            if(answer == "y")
+            {
+                tf = new HtmlTextFormatter();
+            }
+            else
+            {
+                tf = new PlainTextFormatter();
+            }
+
+            StoryTeller st = new StoryTeller(tf);
             st.WriteHeading1("Snow White and the Wolf");
             st.WriteHeading2("She's leaving home");
             st.WriteParagraph("Once upon a time, in a land far, far away...");
